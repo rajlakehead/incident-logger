1c800f0edda53b8c68421e547a7d3fc702beeb88
Raj Patel <patelraj24365@gmail.com>
2025-07-28 13:45:11 -0400

    changed db url

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,9 @@
 import pg from 'pg';
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('postgresql://neondb_owner:npg_b2tShYprPH6B@ep-billowing-king-aeqt7n2c-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require', {
+const connectionString = process.env.DATABASE_URL || '';
+
+export const sequelize = new Sequelize(connectionString, {
   dialect: 'postgres',
   dialectModule: pg
 });
